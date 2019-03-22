604730fb050529c684b04c6a12ba1acc04dde581
Randy Heiland <heiland@indiana.edu>
2019-03-22 15:31:46 -0400

    add About tab; title; fix bugs

--- bin/pcISA.py
+++ bin/pcISA.py
@@ -1,17 +1,28 @@
 import ipywidgets as widgets
-from hublib.ui import RunCommand, Submit
 import xml.etree.ElementTree as ET  # https://docs.python.org/2/library/xml.etree.elementtree.html
 import os
 import glob
 import shutil
+import math
 import datetime
 import tempfile
+from about import AboutTab
 from config import ConfigTab
 from user_params import UserTab
 from svg import SVGTab
 from substrates import SubstrateTab
 from pathlib import Path
 from debug import debug_view
+import platform
+
+hublib_flag = True
+if platform.system() != 'Windows':
+    try:
+#        print("Trying to import hublib.ui")
+        from hublib.ui import RunCommand, Submit
+    except:
+        hublib_flag = False
+
 
 # join_our_list = "(Join/ask questions at https://groups.google.com/forum/#!forum/physicell-users)\n"
 
@@ -20,10 +31,14 @@ tab_layout = widgets.Layout(width='auto',   # border='2px solid black',
                             height=tab_height, overflow_y='scroll',)
 
 # create the tabs, but don't display yet
+about_tab = AboutTab()
 config_tab = ConfigTab()
 
-full_filename = os.path.abspath('data/PhysiCell_settings.xml')
-tree = ET.parse(full_filename)  # this file cannot be overwritten; part of tool distro
+#full_filename = os.path.abspath('data/PhysiCell_settings.xml')
+xml_file = os.path.join('data', 'PhysiCell_settings.xml')
+full_xml_filename = os.path.abspath(xml_file)
+
+tree = ET.parse(full_xml_filename)  # this file cannot be overwritten; part of tool distro
 xml_root = tree.getroot()
 user_tab = UserTab()
 svg = SVGTab()
@@ -37,8 +52,6 @@ if( 'HOME' in os.environ.keys() ):
 def read_config_cb(_b):
     # with debug_view:
     #     print("read_config_cb", read_config.value)
-        # e.g.  "DEFAULT" -> read_config /Users/heiland/dev/pcISA/data/nanobio_settings.xml
-        #       "t360.xml" -> read_config /Users/heiland/.local/share/pcISA/t360.xml
 
     if read_config.value is None:  #occurs when a Run just finishes and we update pulldown with the new cache dir??
         # with debug_view:
@@ -72,20 +85,27 @@ def read_config_cb(_b):
         
 # Using the param values in the GUI, write a new .xml config file
 def write_config_file(name):
-    # Read in the default xml config file, just to get a valid 'root' to populate a new one
-    full_filename = os.path.abspath('data/PhysiCell_settings.xml')
+#    xml_file = os.path.join('data', 'PhysiCell_settings.xml')
+#    full_filename = os.path.abspath(xml_file)
     # with debug_view:
     #     print("write_config_file: based on ",full_filename)
-    tree = ET.parse(full_filename)  # this file cannot be overwritten; part of tool distro
+    tree = ET.parse(full_xml_filename)  # this file cannot be overwritten; part of tool distro
     xml_root = tree.getroot()
     config_tab.fill_xml(xml_root)
     user_tab.fill_xml(xml_root)
     tree.write(name)
 
+    # update substrate mesh layout (beware of https://docs.python.org/3/library/functions.html#round)
+    sub.numx =  math.ceil( (config_tab.xmax.value - config_tab.xmin.value) / config_tab.xdelta.value )
+    sub.numy =  math.ceil( (config_tab.ymax.value - config_tab.ymin.value) / config_tab.ydelta.value )
+    # print("------- sub.numx, sub.numy = ", sub.numx, sub.numy)
+
 
 # callback from write_config_button
 def write_config_file_cb(b):
-    dirname = os.path.expanduser('~/.local/share/pcISA')
+    path_to_share = os.path.join('~', '.local','share','pcISA')
+    dirname = os.path.expanduser(path_to_share)
+
     val = write_config_box.value
     if val == '':
         val = write_config_box.placeholder
@@ -95,8 +115,9 @@ def write_config_file_cb(b):
 # Fill the "Load Config" dropdown widget with valid cached results (and 
 # default & previous config options)
 def get_config_files():
-    cf = {'DEFAULT': os.path.abspath('data/PhysiCell_settings.xml')}
-    dirname = os.path.expanduser('~/.local/share/pcISA')
+    cf = {'DEFAULT': full_xml_filename}
+    path_to_share = os.path.join('~', '.local','share','pcISA')
+    dirname = os.path.expanduser(path_to_share)
     try:
         os.makedirs(dirname)
     except:
@@ -107,7 +128,7 @@ def get_config_files():
 
     # Find the dir path (full_path) to the cached dirs
     if nanoHUB_flag:
-        full_path = os.path.expanduser("~/data/results/.submit_cache/pcISA")
+        full_path = os.path.expanduser("~/data/results/.submit_cache/pcISA")  # does Windows like this?
     else:
         # local cache
         try:
@@ -195,7 +216,7 @@ def run_sim_func(s):
     os.makedirs('tmpdir')
 
     # write the default config file to tmpdir
-    new_config_file = "tmpdir/config.xml"
+    new_config_file = "tmpdir/config.xml"  # use Path; work on Windows?
     write_config_file(new_config_file)  
 
     with open(new_config_file) as f:
@@ -208,9 +229,7 @@ def run_sim_func(s):
 
     if nanoHUB_flag:
         if remote_cb.value:
-            # s.run(run_name, "-n 8 -w 1440 pc4nanobio-r77 config.xml")  # will wait in standby queue forever
-            # s.run(run_name, "-w 7200 pc4nanobio-r77 config.xml") 
-            s.run(run_name, "-v ncn-hub_M@brown -n 8 -w 1440 pcISA-r7 config.xml") 
+            s.run(run_name, "-v ncn-hub_M@brown -n 8 -w 1440 pcISA-r7 config.xml")   # "-r7" suffix??
         else:
             # read_config.index = 0   # reset Dropdown 'Load Config' to 'DEFAULT' when Run interactively
             s.run(run_name, "--local ../bin/myproj config.xml")
@@ -233,6 +252,18 @@ def outcb(s):
         sub.update('')
     return s
 
+# Callback for the (dumb) 'Run' button (without hublib.ui)
+def run_button_cb(s):
+#    with debug_view:
+#        print('run_button_cb')
+
+#    new_config_file = "config.xml"
+    new_config_file = full_xml_filename
+    write_config_file(new_config_file)
+#    subprocess.call(["biorobots", xml_file_out])
+#    subprocess.call(["myproj", new_config_file])   # spews to shell, but not ctl-C'able
+#    subprocess.call(["myproj", new_config_file], shell=True)  # no
+    subprocess.Popen(["myproj", new_config_file])
 
 if nanoHUB_flag:
     run_button = Submit(label='Run',
@@ -242,11 +273,20 @@ if nanoHUB_flag:
                         showcache=False,
                         outcb=outcb)
 else:
-    run_button = RunCommand(start_func=run_sim_func,
+    if (hublib_flag):
+        run_button = RunCommand(start_func=run_sim_func,
                             done_func=run_done_func,
                             cachename='pcISA',
                             showcache=False,
                             outcb=outcb)  
+    else:
+        run_button = widgets.Button(
+            description='Run',
+            button_style='success',  # 'success', 'info', 'warning', 'danger' or ''
+            tooltip='Run a simulation',
+        )
+        run_button.on_click(run_button_cb)
+
 
 read_config = widgets.Dropdown(
     description='Load Config',
@@ -268,25 +308,33 @@ read_config.observe(read_config_cb, names='value')
 # )
 # write_config_row = widgets.HBox([write_config_button, write_config_box])
 
-titles = ['Config Basics', 'User Params', 'Cell Plots', 'Substrate Plots']
-tabs = widgets.Tab(children=[config_tab.tab, user_tab.tab, svg.tab, sub.tab],
+titles = ['About', 'Config Basics', 'User Params', 'Out: Cell Plots', 'Out: Substrate Plots']
+tabs = widgets.Tab(children=[about_tab.tab, config_tab.tab, user_tab.tab, svg.tab, sub.tab],
                    _titles={i: t for i, t in enumerate(titles)},
                    layout=tab_layout)
 
 homedir = os.getcwd()
 
+#desc_button6 = Button(description='pcISA', disabled=True, layout=desc_button_layout) 
+#title_button = widgets.Button(description='pcISA', disabled=True) 
+#title_button.style.button_color = 'tan'
+tool_title = widgets.Label(r'\(\textbf{pcISA: Invader-Supplier-Attacker}\)')
 if nanoHUB_flag:
     remote_cb = widgets.Checkbox(indent=False, value=False, description='Submit as Batch Job to Clusters/Grid')
     #gui = widgets.VBox(children=[read_config, tabs, write_config_row, remote_cb, run_button.w])
 
     # Let's not allow for batch runs for this tool.
     # gui = widgets.VBox(children=[read_config, tabs, remote_cb, run_button.w])
-    gui = widgets.VBox(children=[read_config, tabs, run_button.w])
+    top_row = widgets.HBox(children=[read_config, tool_title])
+#    gui = widgets.VBox(children=[read_config, tabs, run_button.w])
+    gui = widgets.VBox(children=[top_row, tabs, run_button.w])
     #gui = widgets.VBox(children=[tabs, run_button.w])
 else:
     #gui = widgets.VBox(children=[read_config, tabs, write_config_row, run_button.w])
     #gui = widgets.VBox(children=[read_config, tabs, run_button.w])
-    gui = widgets.VBox(children=[tabs, run_button.w])
+    top_row = widgets.HBox(children=[tool_title])
+    gui = widgets.VBox(children=[top_row, tabs, run_button.w])
+
 fill_gui_params(read_config.options['DEFAULT'])
 
 # pass in (relative) directory where output data is located
--- bin/substrates.py
+++ bin/substrates.py
@@ -27,6 +27,10 @@ class SubstrateTab(object):
         self.field_index = 4
         # self.field_index = self.mcds_field.value + 4
 
+        # define dummy size of mesh (set in the tool's primary module)
+        self.numx = 0
+        self.numy = 0
+
         tab_height = '500px'
         constWidth = '180px'
         constWidth2 = '150px'
@@ -184,9 +188,9 @@ class SubstrateTab(object):
         try:
             fname = os.path.join(self.output_dir, "initial.xml")
             tree = ET.parse(fname)
-#            return
+            xml_root = tree.getroot()
         except:
-            print("Cannot open ",fname," to get names of substrate fields.")
+            print("Cannot open ",fname," to read info, e.g., names of substrate fields.")
             return
 
         xml_root = tree.getroot()
@@ -310,7 +314,7 @@ class SubstrateTab(object):
         if not os.path.isfile(full_fname):
 #            print("File does not exist: ", full_fname)
 #            print("No: ", full_fname)
-            print("Missing output file")  # No:  output00000000_microenvironment0.mat
+            print("Once output files are generated, click the slider.")  # No:  output00000000_microenvironment0.mat
 
             return
 
@@ -341,21 +345,43 @@ class SubstrateTab(object):
         #     im = ax.imshow(f.reshape(100,100), interpolation='nearest', cmap=cmap, extent=[0,20, 0,20])
         #     ax.grid(False)
 
-        N = int(math.sqrt(len(M[0,:])))
-        grid2D = M[0, :].reshape(N,N)
-        xvec = grid2D[0, :]
+        # print("substrates.py: ------- numx, numy = ", self.numx, self.numy )
+        if (self.numx == 0):   # need to parse vals from the config.xml
+            fname = os.path.join(self.output_dir, "config.xml")
+            tree = ET.parse(fname)
+            xml_root = tree.getroot()
+            xmin = float(xml_root.find(".//x_min").text)
+            xmax = float(xml_root.find(".//x_max").text)
+            dx = float(xml_root.find(".//dx").text)
+            ymin = float(xml_root.find(".//y_min").text)
+            ymax = float(xml_root.find(".//y_max").text)
+            dy = float(xml_root.find(".//dy").text)
+            self.numx =  math.ceil( (xmax - xmin) / dx)
+            self.numy =  math.ceil( (ymax - ymin) / dy)
+
+        xgrid = M[0, :].reshape(self.numy, self.numx)
+        ygrid = M[1, :].reshape(self.numy, self.numx)
 
         num_contours = 15
-#        levels = MaxNLocator(nbins=10).tick_values(vmin, vmax)
         levels = MaxNLocator(nbins=num_contours).tick_values(self.cmap_min.value, self.cmap_max.value)
+        contour_ok = True
         if (self.cmap_fixed.value):
-            my_plot = plt.contourf(xvec, xvec, M[self.field_index, :].reshape(N,N), levels=levels, extend='both', cmap=self.field_cmap.value)
+            try:
+                my_plot = plt.contourf(xgrid, ygrid, M[self.field_index, :].reshape(self.numy, self.numx), levels=levels, extend='both', cmap=self.field_cmap.value)
+            except:
+                contour_ok = False
+                # print('got error on contourf 1.')
         else:    
-#        my_plot = plt.contourf(xvec, xvec, M[self.field_index, :].reshape(N,N), num_contours, cmap=self.field_cmap.value)
-            my_plot = plt.contourf(xvec, xvec, M[self.field_index, :].reshape(N,N), num_contours, cmap=self.field_cmap.value)
+            try:
+                my_plot = plt.contourf(xgrid, ygrid, M[self.field_index, :].reshape(self.numy,self.numx), num_contours, cmap=self.field_cmap.value)
+            except:
+                contour_ok = False
+                # print('got error on contourf 2.')
 
-        plt.title(title_str)
-        plt.colorbar(my_plot)
+        if (contour_ok):
+            plt.title(title_str)
+            plt.colorbar(my_plot)
+
         axes_min = 0
         axes_max = 2000
         # plt.xlim(axes_min, axes_max)
